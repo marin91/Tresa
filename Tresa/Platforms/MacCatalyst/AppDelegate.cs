0e939b0cf10a84f3455cbbd5a75541e64b12ab16
mmarin <mmarin@OCMMARIN>
2025-10-18 07:00:01 -0700

    Add project files.

---
+++ Tresa/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tresa
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
