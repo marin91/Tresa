55cc78314fe774d4e4b65016f2e294571b0293fd
Miguel Marin <mmarin@costar.com>
2025-10-18 14:23:18 -0700

    Added the first ViewModel type.

---
+++ Tresa/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Tresa.ViewModels;
+
+internal class MainViewModel : INotifyPropertyChanged
+{
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
