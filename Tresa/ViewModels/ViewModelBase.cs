07f2e5ab3f250aea415254e53742c2e3adeda9c4
Miguel Marin <mmarin@costar.com>
2025-10-23 13:20:42 -0700

    Deleted the view model base class that is no longer needed.

--- Tresa/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tresa.ViewModels
-{
-    public abstract class ViewModelBase
-    {
-        protected Task NavigateAsync(string route) =>
-            MainThread.InvokeOnMainThreadAsync(() => Shell.Current.GoToAsync(route));
-    }
-}
